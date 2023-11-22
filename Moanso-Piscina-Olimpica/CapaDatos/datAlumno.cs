7c7999683fbf7cb6b6a435f96d17f9c198f53441
Adrian Cuba <140936172+Iamnuvbot@users.noreply.github.com>
2023-11-22 00:41:31 -0500

    Capa Datos

---
+++ Moanso-Piscina-Olimpica/CapaDatos/datAlumno.cs
@@ -0,0 +1,139 @@
+﻿using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace CapaDatos
+{
+    public class datAlumno
+    {
+        #region sigleton
+        private static readonly datAlumno _instancia = new datAlumno();
+        public static datAlumno Instancia
+        {
+            get
+            {
+                return datAlumno._instancia;
+            }
+        }
+        #endregion singleton
+        #region metodos
+        public List<entAlumno> ListarAlumnos()
+        {
+            SqlCommand cmd = null;
+            List<entAlumno> lista = new List<entAlumno>();
+            try
+            {
+                SqlConnection cn = Conexion.Instancia.Conectar(); //singleton
+                cmd = new SqlCommand("spListarAlumnos", cn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cn.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    entAlumno Alumno = new entAlumno();
+                    Alumno.idAlumno = Convert.ToInt32(dr["ID"]);
+                    Alumno.DNI = dr["DNI"].ToString();
+                    Alumno.Nombre = dr["Nombre"].ToString();
+                    Alumno.Apellido = dr["Apellido"].ToString();
+                    Alumno.Categoria = dr["Categoria"].ToString();
+                    Alumno.Horario = dr["Horario"].ToString();
+                    Alumno.AsistenciasDisponibles = Convert.ToInt32(dr["AsistenciasDisponibles"]);
+                    Alumno.Correo = dr["Correo"].ToString();
+                    Alumno.Telefono = dr["Telefono"].ToString();
+                    Alumno.Direccion = dr["Direccion"].ToString();
+                    Alumno.Edad = Convert.ToInt32(dr["Edad"]);
+                    Alumno.Estado = Convert.ToBoolean(dr["Estado"]);
+                    Alumno.Nivel = dr["Nivel"].ToString();
+                    lista.Add(Alumno);
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally { cmd.Connection.Close(); }
+            return lista;
+        }
+        public entAlumno ObtenerAlumno(string dni)
+        {
+            entAlumno Alumno = new entAlumno();
+            try
+            {
+                using (SqlConnection cn = Conexion.Instancia.Conectar())
+                using (SqlCommand cmd = new SqlCommand("spObtenerAlumno", cn))
+                {
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddWithValue("@AlumnoDNI", dni);
+
+                    cn.Open();
+
+                    using (SqlDataReader dr = cmd.ExecuteReader())
+                    {
+                        while (dr.Read())
+                        {
+                           
+                            Alumno.DNI = dr["DNI"].ToString();
+                            Alumno.Nombre = dr["Nombre"].ToString();
+                            Alumno.Apellido = dr["Apellido"].ToString();
+                            Alumno.Categoria = dr["Categoria"].ToString();
+                            Alumno.Horario = dr["Horario"].ToString();
+                            Alumno.AsistenciasDisponibles = Convert.ToInt32(dr["AsistenciasDisponibles"]);
+                            Alumno.Correo = dr["Correo"].ToString();
+                            Alumno.Telefono = dr["Telefono"].ToString();
+                            Alumno.Direccion = dr["Direccion"].ToString();
+                            Alumno.Edad = Convert.ToInt32(dr["Edad"]);
+                            Alumno.Estado = Convert.ToBoolean(dr["Estado"]);
+                            Alumno.EstaDentro = Convert.ToBoolean(dr["EstaDentro"]);
+                            Alumno.Nivel = dr["Nivel"].ToString();
+                        }
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+
+            return Alumno;
+        }
+
+        public Boolean InsertarAlumno(entAlumno Alumno)
+        {
+            SqlCommand cmd = null;
+            Boolean inserta = false;
+            try
+            {
+
+                SqlConnection cn = Conexion.Instancia.Conectar();
+                cmd = new SqlCommand("spRegistrarNuevoAlumno", cn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@DNI", Alumno.DNI);
+                cmd.Parameters.AddWithValue("@Nombre", Alumno.Nombre);
+                cmd.Parameters.AddWithValue("@Apellido", Alumno.Apellido);
+                cmd.Parameters.AddWithValue("@Categoria", Alumno.Categoria);
+                cmd.Parameters.AddWithValue("@Horario", Alumno.Horario);
+                cmd.Parameters.AddWithValue("@AsistenciasDisponibles", Alumno.AsistenciasDisponibles);
+                cmd.Parameters.AddWithValue("@Correo", Alumno.Correo);
+                cmd.Parameters.AddWithValue("@Telefono", Alumno.Telefono);
+                cmd.Parameters.AddWithValue("@Direccion", Alumno.Direccion);
+                cmd.Parameters.AddWithValue("@Edad", Alumno.Edad);
+                cmd.Parameters.AddWithValue("@Nivel", Alumno.Nivel);
+                cn.Open();
+                int i = cmd.ExecuteNonQuery();
+                if (i > 0)
+                {
+                    inserta = true;
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally { cmd.Connection.Close(); }
+            return inserta;
+        }
+        #endregion metodos
+    }
+}
---
+++ Moanso-Piscina-Olimpica/CapaDatos/datAsistencia.cs
@@ -0,0 +1,110 @@
+﻿using CapaEntidad;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CapaDatos
+{
+    public class datAsistencia
+    {
+        #region sigleton
+        private static readonly datAsistencia _instancia = new datAsistencia();
+        //privado para evitar la instanciación directa
+        public static datAsistencia Instancia
+        {
+            get
+            {
+                return datAsistencia._instancia;
+            }
+        }
+        #endregion singleton
+        #region metodos
+        public List<entAsistenciaAlumnos> ListarAsistencias(string dni)
+        {
+            SqlCommand cmd = null;
+            List<entAsistenciaAlumnos> lista = new List<entAsistenciaAlumnos>();
+            try
+            {
+                SqlConnection cn = Conexion.Instancia.Conectar(); //singleton
+                cmd = new SqlCommand("spListarAsistenciaAlumno", cn);
+
+                cmd.CommandType = CommandType.StoredProcedure;           
+                cmd.Parameters.AddWithValue("@DNI", dni);
+                cn.Open();
+                SqlDataReader dr = cmd.ExecuteReader();
+                while (dr.Read())
+                {
+                    entAsistenciaAlumnos Asist = new entAsistenciaAlumnos();
+                    Asist.AlumnoDNI = dr["AlumnoDNI"].ToString();
+                    Asist.Fecha = dr["Fecha"].ToString();
+                    Asist.HoraEntrada = dr["HoraEntrada"].ToString();
+                    Asist.HoraSalida= dr["HoraSalida"].ToString();
+                    lista.Add(Asist);
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally { cmd.Connection.Close(); }
+            return lista;
+        }
+        public Boolean InsertarAsistencia(entAsistenciaAlumnos Alumno)
+        {
+            SqlCommand cmd = null;
+            Boolean inserta = false;
+            try
+            {
+                SqlConnection cn = Conexion.Instancia.Conectar();
+                cmd = new SqlCommand("spRegistrarAsistenciaAlumno", cn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@DNI", Alumno.AlumnoDNI);
+                cmd.Parameters.AddWithValue("@Fecha", Alumno.Fecha);
+                cmd.Parameters.AddWithValue("@HoraEntrada", Alumno.HoraEntrada);
+                cn.Open();
+                int i = cmd.ExecuteNonQuery();
+                if (i > 0)
+                {
+                    inserta = true;
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally { cmd.Connection.Close(); }
+            return inserta;
+        }
+        public Boolean InsertarSalida(string dni,string horaSalida)
+        {
+            SqlCommand cmd = null;
+            Boolean inserta = false;
+            try
+            {
+                SqlConnection cn = Conexion.Instancia.Conectar();
+                cmd = new SqlCommand("spRegistrarSalidaAlumno", cn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                cmd.Parameters.AddWithValue("@HoraSalida",horaSalida);
+                cmd.Parameters.AddWithValue("@DNI", dni);
+                cn.Open();
+                int i = cmd.ExecuteNonQuery();
+                if (i > 0)
+                {
+                    inserta = true;
+                }
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+            finally { cmd.Connection.Close(); }
+            return inserta;
+        }
+        #endregion metodos
+    }
+}
+

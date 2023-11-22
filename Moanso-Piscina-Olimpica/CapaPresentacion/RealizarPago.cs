6696c9852f0734ec98f529c2b013aaef75b06ab9
Iamnuvbot <agcg.cuba@gmail.com>
2023-11-22 08:13:11 -0500

    Implementacion finalizada de recibos

--- Moanso-Piscina-Olimpica/CapaPresentacion/RealizarPago.cs
+++ Moanso-Piscina-Olimpica/CapaPresentacion/RealizarPago.cs
@@ -1,19 +1,14 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows.Forms;
-using CapaEntidad;
+﻿using CapaEntidad;
 using CapaLogica;
+using System;
+using System.Linq;
+using System.Windows.Forms;
 
 namespace Moanso_Piscina
 {
     public partial class RealizarPago : Form
     {
+        static int idBoleta = 0;
         public RealizarPago()
         {
             InitializeComponent();
@@ -26,12 +21,19 @@ namespace Moanso_Piscina
             string Fecha = fechaBox.Value.ToString();
             string TipoServicio = cbBTipoServicio.SelectedItem.ToString();
             string DNI = textBox_DNI.Text;
+            entBoleta ultimo = logBoleta.Instancia.ListarBoletas().LastOrDefault();
+            if (ultimo != null)
+            {
+                idBoleta = ultimo.idBoleta;
+            }
+            idBoleta++;
             entBoleta boleta = new entBoleta();
             boleta.DNI = DNI;
             boleta.MetododePago = MetododePago;
             boleta.Monto = Monto;
             boleta.Fecha = Fecha;
             boleta.TipoServicio = TipoServicio;
+            boleta.idBoleta = idBoleta;
             logBoleta.Instancia.InsertarBoleta(boleta);
             SetDtgv();
             richTextBox_MostrarBoleta.Text = $"BOLETA EMITIDA EXITOSAMENTE\n" +
@@ -59,15 +61,31 @@ namespace Moanso_Piscina
         }
         private void btn_AnularBoletas_Click(object sender, EventArgs e)
         {
-            string Fecha = dataGridView1.SelectedRows[0].Cells[2].Value.ToString();
-            if (Fecha != null)
-            {
-                logBoleta.Instancia.AnularBoleta(Fecha);
-            }
-            else
-            {
+            logBoleta.Instancia.AnularBoleta(valorFecha);
+        }
+        private void dataGridView1_CellContentDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            // Obtener el índice de la fila seleccionada
+            int fila = e.RowIndex;
 
-            }
+            // Obtener el valor de la primera columna de la fila seleccionada
+            string valor = dataGridView1.Rows[fila].Cells[2].Value.ToString();
+
+            // Mostrar el valor en un MessageBox o hacer lo que quieras con él
+            valorFecha = valor;
+        }
+        private string valorFecha = "";
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+            // Obtener el índice de la fila seleccionada
+            int fila = e.RowIndex;
+
+            // Obtener el valor de la primera columna de la fila seleccionada
+            string valor = dataGridView1.Rows[fila].Cells[2].Value.ToString();
+
+            // Mostrar el valor en un MessageBox o hacer lo que quieras con él
+            valorFecha = valor;
         }
     }
 }
